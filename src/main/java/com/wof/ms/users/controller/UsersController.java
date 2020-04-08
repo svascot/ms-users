82fa2665282f40b2b94dd7f9963a8fe0ca65710c
Santiago Vasco Tabares <santiago.vasco@globant.com>
2020-04-08 02:03:04 -0500

    Fixing endpoint get by id

--- src/main/java/com/wof/ms/users/controller/UsersController.java
+++ src/main/java/com/wof/ms/users/controller/UsersController.java
@@ -3,14 +3,13 @@ package com.wof.ms.users.controller;
 import com.wof.ms.users.domain.interfaces.IUserService;
 import com.wof.ms.users.model.User;
 import java.net.URI;
-import java.util.Arrays;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController extends AbstractController<IUserService>{
 
   public static final String API_PATH = "/users";
+
   /**
    * Call the method that return a list of User objects.
    * @return an List of User.
@@ -32,21 +32,36 @@ public class UsersController extends AbstractController<IUserService>{
     return ResponseEntity.ok(service.getAll());
   }
 
+  /**
+   * Call the method that return an User object by the ID provided.
+   * @param id Is the id of the user.
+   * @return an List of User.
+   */
   @GetMapping("/{id}")
-  public ResponseEntity get(@RequestParam String id) {
+  public ResponseEntity get(@PathVariable int id) {
     try{
-      return ResponseEntity.ok(service.get(Integer.valueOf(id)));
+      return ResponseEntity.ok(service.get(id));
     }catch (Exception e){
       return ResponseEntity.badRequest().body(e.getMessage());
     }
   }
 
+  /**
+   * Call the method that create a new User.
+   * @param user is the user object that will be created.
+   * @return an Uri to find the user.
+   */
   @PostMapping
   public ResponseEntity create(@RequestBody User user){
     final URI uri = URI.create(API_PATH + "/" +service.create(user).getId());
-    return ResponseEntity.created(uri).build();
+    return ResponseEntity.ok().body(uri);
   }
 
+  /**
+   * Call the method that pdate an User.
+   * @param user is the user object that contains the new information.
+   * @return an Uri to find the user.
+   */
   @PutMapping
   public ResponseEntity update(@RequestBody User user){
     final URI uri = URI.create(API_PATH + "/" +service.update(user).getId());
